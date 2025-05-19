aac977e0170ec876ef17594176b7730b337c9555
OptimistiCompound <2112689509@qq.com>
2025-05-19 22:04:11 +0800

    Delete pipeline_CPU/Header_Files/io_header.v

--- pipeline_CPU/Header_Files/io_header.v
+++
@@ -1,7 +0,0 @@
-
-//io base addr
-`define     LED_BASE_ADDR       32'hFFFFFC60
-`define     SWITCH_BASE_ADDR    32'hFFFFFC64
-`define     KEY_BASE_ADDR       32'hFFFFFC68
-`define     SEG_BASE_ADDR       32'hFFFFFC6C
-`define     Btn_ADDR            32'hFFFFFC70
