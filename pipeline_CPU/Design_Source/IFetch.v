b9010843adc9586c7907b297a454210eea69c917
ZhongYong <2112689509@qq.com>
2025-05-22 23:27:05 +0800

    modified IFetch, add FLush

--- pipeline_CPU/Design_Source/IFetch.v
+++ pipeline_CPU/Design_Source/IFetch.v
@@ -39,15 +39,20 @@ module IFetch(
 
     // Outputs
     output [31:0] inst,
-    output [31:0] pc4_i
+    output [31:0] pc4_i,
+    output FLush
     );
 wire mode = upg_rst_i | (~upg_rst_i & upg_done_i);
 
 reg [31:0] PC;
-wire [31:0] next_PC =   (rstn==0) ? 0 : 
-                        (Branch || Jump) ? PC + imm32 : 
-                        (Jalr) ? ALUResult : 
+wire [31:0] next_PC =   (rstn==0)           ? 0 : 
+                        (Branch || Jump)    ? PC + imm32 : 
+                        (Jalr)              ? ALUResult : 
                         PC + 32'h4;
+assign Flush        =   (Branch || Jump)    ? 1'b1 :
+                        (Jalr)              ? 1'b1 :
+                        1'b0;
+
 always @(negedge clk or negedge rstn) begin
     if (~rstn)
         PC <= 0;
