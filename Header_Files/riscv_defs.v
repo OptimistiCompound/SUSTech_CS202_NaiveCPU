4d30cf83246fdedef081b2c1685c2c7c004d1dec
Van <2112689509@qq.com>
2025-05-22 11:50:18 +0800

    adjust macros

--- Header_Files/riscv_defs.v
+++ Header_Files/riscv_defs.v
@@ -59,7 +59,7 @@
 `define INST_AND                                12'h7_00
 `define INST_SLL                                12'h1_00
 `define INST_SRL                                12'h5_00
-`define INST_SRA                                12'h5_00
+`define INST_SRA                                12'h5_20
 `define INST_SLT                                12'h2_00
 `define INST_SLTU                               12'h3_00
 
