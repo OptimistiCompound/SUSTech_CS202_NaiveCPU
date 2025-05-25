8382b270413adc83a697f7e449f84e09bca11290
ZhongYong <2112689509@qq.com>
2025-05-26 05:01:18 +0800

    add description to Debug_COntroller

--- Design_Sources/Debug_Controller.v
+++ Design_Sources/Debug_Controller.v
@@ -7,6 +7,16 @@ module Debug_Controller(
     output reg clk_out,
     output reg mode
 );
+
+// 何时进入debug模式？
+//      1. eBreak
+//      2. eRead
+// 何时退出debug模式？
+//      1. 按下btn1
+// 在debug模式下，CPU的行为
+//      1. 按下btn2，时钟行进一个clk_in周期然后clk_out变为0
+//      2. 可以通过switch和conf_btn查询寄存器的值，显示在数码管上
+
 always @(posedge clk or negedge rstn) begin
     if (~rstn)begin
         mode = 0;
